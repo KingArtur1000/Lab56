7a7f6877f2d301fed05484087596c6a5fdcf8372
Артур Александрович <artur.dm.2005@yandex.ru>
2026-09-09 22:48:05 +0300

    return string values

--- app/src/main/java/com/kingartur1000/lab56/MainActivity.kt
+++ app/src/main/java/com/kingartur1000/lab56/MainActivity.kt
@@ -57,7 +57,7 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.btnDelete)?.setOnClickListener {
             if (expression.isNotEmpty()) {
                 expression = expression.substring(0, expression.length - 1)
-                tvExpression.text = if (expression.isEmpty()) "0" else expression
+                tvExpression.text = expression.ifEmpty { "0" }
                 updatePreviewResult()
             }
         }
