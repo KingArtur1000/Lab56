9a7c590b324fc33559faadf7f7268c0ef34f5bba
Артур Александрович <artur.dm.2005@yandex.ru>
2026-09-09 22:59:55 +0300

    Добавлена исправление для чисел с плавающей точкой в CalculatorEngine.kt

--- app/src/main/java/com/kingartur1000/lab56/CalculatorEngine.kt
+++ app/src/main/java/com/kingartur1000/lab56/CalculatorEngine.kt
@@ -1,5 +1,8 @@
 package com.kingartur1000.lab56
 
+import java.math.BigDecimal
+import java.math.MathContext
+import java.math.RoundingMode
 import kotlin.math.acos
 import kotlin.math.asin
 import kotlin.math.atan
@@ -13,7 +16,11 @@ import kotlin.math.tan
 
 object CalculatorEngine {
 
-    fun evaluate(expression: String): Double {
+    private val mathContext = MathContext(15, RoundingMode.HALF_UP)
+    // Контекст с точностью 12 цифр для сглаживания погрешностей Double и Math.PI
+    private val trigMathContext = MathContext(12, RoundingMode.HALF_UP)
+
+    fun evaluate(expression: String): BigDecimal {
         val str = expression
             .replace("×", "*")
             .replace("÷", "/")
@@ -21,7 +28,7 @@ object CalculatorEngine {
             .replace("π", Math.PI.toString())
             .replace("E", Math.E.toString())
 
-        val result = object : Any() {
+        return object : Any() {
             var pos = -1
             var ch = 0
 
@@ -38,37 +45,47 @@ object CalculatorEngine {
                 return false
             }
 
-            fun parse(): Double {
+            fun parse(): BigDecimal {
                 nextChar()
                 val x = parseExpression()
                 if (pos < str.length) throw RuntimeException("Неожиданный символ: " + ch.toChar())
                 return x
             }
 
-            fun parseExpression(): Double {
+            fun parseExpression(): BigDecimal {
                 var x = parseTerm()
                 while (true) {
-                    if (eat('+'.code)) x += parseTerm()
-                    else if (eat('-'.code)) x -= parseTerm()
+                    if (eat('+'.code)) x = x.add(parseTerm(), mathContext)
+                    else if (eat('-'.code)) x = x.subtract(parseTerm(), mathContext)
                     else return x
                 }
             }
 
-            fun parseTerm(): Double {
+            fun parseTerm(): BigDecimal {
                 var x = parseFactor()
                 while (true) {
-                    if (eat('*'.code)) x *= parseFactor()
-                    else if (eat('/'.code)) x /= parseFactor()
-                    else if (eat('%'.code)) x %= parseFactor()
-                    else return x
+                    if (eat('*'.code)) {
+                        x = x.multiply(parseFactor(), mathContext)
+                    } else if (eat('/'.code)) {
+                        val divisor = parseFactor()
+                        if (divisor.compareTo(BigDecimal.ZERO) == 0) {
+                            throw ArithmeticException("Деление на ноль")
+                        }
+                        x = x.divide(divisor, mathContext)
+                    } else if (eat('%'.code)) {
+                        val divisor = parseFactor()
+                        x = x.remainder(divisor, mathContext)
+                    } else {
+                        return x
+                    }
                 }
             }
 
-            fun parseFactor(): Double {
+            fun parseFactor(): BigDecimal {
                 if (eat('+'.code)) return parseFactor()
-                if (eat('-'.code)) return -parseFactor()
+                if (eat('-'.code)) return parseFactor().negate()
 
-                var x: Double
+                var x: BigDecimal
                 val startPos = pos
 
                 if (eat('('.code)) {
@@ -76,48 +93,56 @@ object CalculatorEngine {
                     eat(')'.code)
                 } else if ((ch in '0'.code..'9'.code) || ch == '.'.code) {
                     while ((ch in '0'.code..'9'.code) || ch == '.'.code) nextChar()
-                    x = str.substring(startPos, pos).toDouble()
+                    x = BigDecimal(str.substring(startPos, pos))
                 } else if (ch in 'a'.code..'z'.code || ch in 'A'.code..'Z'.code || ch == '√'.code) {
                     while (ch in 'a'.code..'z'.code || ch in 'A'.code..'Z'.code || ch == '√'.code) nextChar()
                     val func = str.substring(startPos, pos)
-                    x = parseFactor()
-                    x = when (func) {
-                        "sin" -> sin(Math.toRadians(x))
-                        "cos" -> cos(Math.toRadians(x))
-                        "tan" -> tan(Math.toRadians(x))
-                        "asin" -> Math.toDegrees(asin(x))
-                        "acos" -> Math.toDegrees(acos(x))
-                        "atan" -> Math.toDegrees(atan(x))
-                        "lg" -> log10(x)
-                        "ln" -> ln(x)
-                        "√" -> sqrt(x)
+                    val arg = parseFactor()
+                    val argDouble = arg.toDouble()
+
+                    val resDouble = when (func) {
+                        "sin" -> sin(Math.toRadians(argDouble))
+                        "cos" -> cos(Math.toRadians(argDouble))
+                        "tan" -> tan(Math.toRadians(argDouble))
+                        "asin" -> Math.toDegrees(asin(argDouble))
+                        "acos" -> Math.toDegrees(acos(argDouble))
+                        "atan" -> Math.toDegrees(atan(argDouble))
+                        "lg" -> log10(argDouble)
+                        "ln" -> ln(argDouble)
+                        "√" -> sqrt(argDouble)
                         else -> throw RuntimeException("Неизвестная функция: $func")
                     }
+
+                    if (resDouble.isNaN() || resDouble.isInfinite()) {
+                        throw ArithmeticException("Математическая ошибка")
+                    }
+
+                    // Округление до 12 значащих цифр мягко сглаживает 0.49999999999999994 до 0.5
+                    x = BigDecimal.valueOf(resDouble).round(trigMathContext)
                 } else {
                     throw RuntimeException("Неожиданный символ: " + ch.toChar())
                 }
 
-                if (eat('^'.code)) x = x.pow(parseFactor())
+                if (eat('^'.code)) {
+                    val exponent = parseFactor()
+                    val resDouble = x.toDouble().pow(exponent.toDouble())
+                    x = BigDecimal.valueOf(resDouble).round(trigMathContext)
+                }
                 if (eat('!'.code)) {
-                    var fact = 1.0
-                    for (i in 1..x.toInt()) fact *= i
+                    var fact = BigDecimal.ONE
+                    val n = x.toInt()
+                    for (i in 1..n) {
+                        fact = fact.multiply(BigDecimal.valueOf(i.toLong()))
+                    }
                     x = fact
                 }
                 return x
             }
         }.parse()
-
-        if (result.isNaN() || result.isInfinite()) {
-            throw ArithmeticException("Математическая ошибка")
-        }
-        return result
     }
 
-    fun formatResult(result: Double): String {
-        return if (result % 1.0 == 0.0) {
-            result.toLong().toString()
-        } else {
-            result.toString().replace('.', ',')
-        }
+    fun formatResult(result: BigDecimal): String {
+        if (result.compareTo(BigDecimal.ZERO) == 0) return "0"
+        return result.stripTrailingZeros().toPlainString().replace('.', ',')
     }
 }
