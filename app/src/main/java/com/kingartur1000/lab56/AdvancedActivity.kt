4b6fcda51ed29d8b823b5af851f0a6bd1b17106b
Артур Александрович <artur.dm.2005@yandex.ru>
2026-09-09 21:15:17 +0300

    added activity_advanced.xml

--- app/src/main/java/com/kingartur1000/lab56/AdvancedActivity.kt
+++ app/src/main/java/com/kingartur1000/lab56/AdvancedActivity.kt
@@ -1,14 +1,26 @@
 package com.kingartur1000.lab56
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class AdvancedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Временно подключим тот же макет или пустой.
-        // Для полноценной сдачи лабы №5 здесь нужно будет создать activity_advanced.xml
-        // с дополнительными кнопками (sin, cos, и т.д.)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_advanced)
+
+        // Кнопка возврата на стандартный калькулятор
+        findViewById<ImageButton>(R.id.btnSwitchBack).setOnClickListener {
+                view ->
+            view.animate()
+                .scaleX(0.9f).scaleY(0.9f).setDuration(50)
+                .withEndAction {
+                    view.animate().scaleX(1f).scaleY(1f).setDuration(50).start()
+                    finish() // Закрываем Activity, возвращаясь на MainActivity
+                }.start()
+        }
+
+        // TODO: Здесь нужно будет добавить слушатели для новых кнопок (sin, cos, π)
+        // и адаптировать парсер математических выражений.
     }
 }
